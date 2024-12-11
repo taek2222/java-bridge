8a8fb906a4a0886074bfc23b4d5f76cf50a8bfc2
taek2222 <oot309@daum.net>
2024-12-11 21:02:47 +0900

    feat: 게임 시작 문구 출력

--- src/main/java/bridge/controller/BridgeGame.java
+++ src/main/java/bridge/controller/BridgeGame.java
@@ -1,9 +1,26 @@
 package bridge.controller;
 
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BridgeGame(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        outputView.printGameStart();
+
+
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
