d9080f6d340d408fc55dd271ee7033bf69c3268d
taek2222 <oot309@daum.net>
2024-12-12 01:50:09 +0900

    remove: 사용하지 않는 메서드 삭제

--- src/main/java/bridge/global/validation/CommonValidator.java
+++ src/main/java/bridge/global/validation/CommonValidator.java
@@ -4,30 +4,11 @@ import bridge.global.constant.ErrorMessage;
 
 public class CommonValidator {
 
-    public static void validateNotBlank(String input) {
-        if (input == null || input.isBlank()) {
-            throw new IllegalArgumentException();
-        }
-    }
-
     public static void validateIsNumeric(String input) { //
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_NUMERIC.get());
-        }
-    }
-
-    public static void validateRange(int value, int min, int max) {
-        if (value < min || value > max) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    public static void validateLength(String input, int minLen, int maxLen) {
-        int length = input.length();
-        if (length < minLen || length > maxLen) {
-            throw new IllegalArgumentException();
         }
     }
 }
