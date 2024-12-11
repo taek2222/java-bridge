24c97a4571c4f7466b4d5ea12df690e5fffadc54
taek2222 <oot309@daum.net>
2024-12-12 02:38:33 +0900

    test: 다리 전진 가능/불가능 테스트 설계

--- src/test/java/bridge/domain/BridgeMakerTest.java
+++ src/test/java/bridge/domain/BridgeMakerTest.java
@@ -1,13 +1,53 @@
 package bridge.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import bridge.global.constant.ErrorMessage;
 import bridge.global.util.BridgeRandomNumberGenerator;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class BridgeMakerTest {
+
+    @Test
+    void 다리_전진이_계속_가능하면_TRUE를_반환한다() {
+        // given
+        String up = "U";
+        String down = "D";
+        List<String> answer = List.of(up, down, down);
+
+        Bridge bridge = new Bridge(answer);
+        bridge.addMoving(up);
+        bridge.addMoving(down);
+
+        // when
+        boolean result = bridge.isPossible();
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void 다리_전진이_불가능하면_FALSE를_반환한다() {
+        // given
+        String up = "U";
+        String down = "D";
+        List<String> answer = List.of(up, down, down);
+
+        Bridge bridge = new Bridge(answer);
+        bridge.addMoving(up);
+        bridge.addMoving(down);
+        bridge.addMoving(down);
+
+        // when
+        boolean result = bridge.isPossible();
+
+        // then
+        assertThat(result).isFalse();
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {2, 21})
