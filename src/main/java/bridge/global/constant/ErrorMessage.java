4a13ea144d90433672f94dc666552315fb7cf786
taek2222 <oot309@daum.net>
2024-12-12 01:50:19 +0900

    remove: 사용하지 않는 에러 삭제

--- src/main/java/bridge/global/constant/ErrorMessage.java
+++ src/main/java/bridge/global/constant/ErrorMessage.java
@@ -2,10 +2,7 @@ package bridge.global.constant;
 
 public enum ErrorMessage {
     INVALID_INPUT("유효 하지 않은 입력 입니다."),
-//    INVALID_FORMAT("입력값의 형식이 잘못되었습니다."),
     OUT_OF_BRIDGE_SIZE_RANGE("다리 길이는 %d부터 %d 사이의 숫자여야 합니다."),
-//    INVALID_LENGTH("입력값의 길이가 잘못되었습니다. (%d ~ %d자)"),
-//    DUPLICATE_ENTRY("중복된 값이 존재합니다."),
     INVALID_NUMERIC("숫자만 입력 가능합니다."),
     ;
 
